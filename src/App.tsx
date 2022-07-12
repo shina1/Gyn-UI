2fab285e3abc50f349273c85c81bcfe2ca31b0eb
shina1 <oswasiu@gmail.com>
2022-07-12 12:50:41 +0100

    completed program section UI implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'
 import HeroSection from './components/Hero-Section';
+import ProgramsComponent from './components/Program-Componenet';
 
 function App() {
   return (
     <div className='App'>
       <HeroSection />
+      <ProgramsComponent />
     </div>
   );
 }
